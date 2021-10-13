7d736ada68d2e33384bc720ae40443acae1f595c
CNwangbin <401030920010@email.ncu.edu.cn>
2021-10-13 21:22:38 +0800

    update conf.py,add import

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','scipy.stats','scipy.sparse']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate','scipy.stats','scipy.sparse','pandas']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
